ee6bec3e32a6f61cf7eedda20012b64b8f276465
Kadir Bay <kadirbay13@gmail.com>
2024-08-04 10:34:23 +0300

    message lenth control added

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -9,8 +9,20 @@
 
         <!-- new message card -->
         <div class="input-container flex p-2 border-t border-gray-300">
-            <input v-model="newMessage" @keyup.enter="sendMessage" placeholder="Type a message..." class="flex-1 p-2 border border-gray-300 rounded mr-2" />
-            <button @click="sendMessage" class="p-2 bg-blue-500 text-white rounded">Send</button>
+            <input 
+                v-model="newMessage" 
+                @keyup.enter="sendMessage" 
+                placeholder="Type a message..." 
+                class="flex-1 p-2 border border-gray-300 rounded mr-2" 
+            />
+            <button 
+                @click="sendMessage" 
+                :disabled="newMessage.length < minNewMessageLenght"
+                :class="{ 'text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed rounded-lg text-center': newMessage.length < minNewMessageLenght }"
+                class="p-2 bg-blue-500 text-white rounded"
+            >
+                Send
+            </button>
         </div>
     </div>
 </template>
@@ -31,6 +43,7 @@ export default {
     data() {
         return {
             newMessage: '',
+            minNewMessageLenght: 10,
         }
     },
     mounted() {
