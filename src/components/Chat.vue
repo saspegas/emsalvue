3d1202612bb3db05b8ab8faf171d64ccac5b71fb
Kadir Bay <kadirbay13@gmail.com>
2024-08-05 14:54:04 +0300

    toogle and refresh process added, every five seconds

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -2,6 +2,14 @@
 <template>
     <div class="chat-card border border-gray-300 rounded-lg w-full max-w-lg mx-auto flex flex-col h-500px mt-2 sm:mt-4 md:mt-6 lg:mt-8">
         
+        <!-- Refresh buttons -->
+        <div class="flex justify-end p-2">
+            <button @click="getMessages" class="p-2 bg-orange-300 text-white rounded mr-2">Refresh Now</button>
+            <button @click="toggleAutoRefresh" :class="{'bg-red-500': autoRefresh, 'bg-blue-500': !autoRefresh}" class="p-2 text-white rounded">
+                {{ autoRefresh ? 'Stop Auto Refresh' : 'Auto Refresh' }}
+            </button>
+        </div>
+
         <!-- delivered messages -->
         <div v-if="messages.length > 0" ref="messagesContainer" class="flex-1 overflow-y-auto p-4 border-b border-gray-300">
             <Message v-for="(message, index) in messages.slice().reverse()" :key="index" :message="message" />
@@ -10,8 +18,14 @@
             No messages yet
         </div>
 
+        <!-- last update time -->
+        <div class="text-left text-gray-500 dark:text-gray-400 italic p-2 ml-1">
+            Last update: {{ lastUpdateTime }}
+        </div>
+
         <!-- new message card -->
         <div class="input-container flex p-2 border-t border-gray-300">
+
             <input 
                 v-model="newMessage" 
                 @keyup.enter="sendMessage" 
@@ -43,6 +57,8 @@ export default {
             messages: [],
             newMessage: '',
             minNewMessageLenght: 10,
+            autoRefresh: false,
+            lastUpdateTime: '',
         }
     },
     created() {
@@ -74,11 +90,22 @@ export default {
         getMessages() {
             axios.get('/api/messages')
                 .then(response => {
+                    this.lastUpdateTime = new Date().toLocaleString();
                     this.messages = response.data.data;
                 })
                 .catch(error => {
                     console.error(error);
                 });
+        },
+        toggleAutoRefresh() {
+            if (this.autoRefresh) {
+                clearInterval(this.autoRefreshInterval);
+            } else {
+                this.autoRefreshInterval = setInterval(() => {
+                    this.getMessages();
+                }, 5000);
+            }
+            this.autoRefresh = !this.autoRefresh;
         }
     }
 }
